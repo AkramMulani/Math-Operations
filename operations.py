988def5d2c2c2c5e2b97416b077767f96aea669a
AkramMulani <akramamulani786@gmail.com>
2023-12-11 09:24:27 +0530

    Debug operations.py

--- operations.py
+++ operations.py
@@ -7,31 +7,41 @@ class Operations:
             for operation in operations.split(','):
                 try:
                     name = operation.split(':')[0].strip()
-                    define = operation.split(':')[1]
+                    define = operation.split(':')[1].replace(',','')
                     self._operations_[name] = define
                 except Exception as e:
                     pass
+            f.close()
 
     def addOperation(self,name: str, sample: str):
         try:
-            a = self._operations_[name]
+            a = self._operations_[name.strip()]
+            # if a:
+                # print(f'{name} already present in list you can use it')
         except KeyError:
-            self._operations_[name] = sample
+            self._operations_[name.strip()] = sample
+            # print(f'{name} added to dictionary')
             with open('operations.txt','a') as f:
                 operation = f'\n{name}:{sample},'
                 f.write(operation)
+                f.close()
+                # print('File content added')
 
     def removeOperation(self,index:int):
         names = list(self._operations_.keys())
         name = names[index]
         try:
-            del self._operations_[name]
+            del self._operations_[name.strip()]
+            # print(f'{name} deleted from dictionary')
             with open('operations.txt','r') as file:
                 lines = file.readlines()
+                file.close()
             if 0<=index<len(lines):
                 del lines[index]
                 with open('operations.txt','w') as file:
                     file.writelines(lines)
+                file.close()
+                # print('File content removed')
             return 1
         except:
             return 0
